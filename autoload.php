afa7ece2a11b83c4996296a402c384eb1273a1f1
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-02-23 16:09:23 -0300

    autoload

--- autoload.php
+++ autoload.php
@@ -7,5 +7,5 @@ spl_autoload_register(function($nome_da_classe)
     $arquivo = BASEDIR . '/' . $nome_da_classe . '.php';
 
     if (file_exists($arquivo)) include $arquivo;
-    else exit('Arquivo não encontrado. Arquivo: ' . $arquivo);
+    else exit('Arquivo não encontrado. Arquivo: ' . $arquivo . '<br />');
 });
