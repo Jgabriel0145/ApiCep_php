db86f8147ad1436ebee979040b4755c4d96d9054
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-03-07 15:51:52 -0300

    atualização da dao

--- DAO/EnderecoDAO.php
+++ DAO/EnderecoDAO.php
@@ -2,7 +2,27 @@
 
 namespace ApiCep\DAO;
 
+use ApiCep\Model\EnderecoModel;
+
 class EnderecoDAO extends DAO
 {
-    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function selectByCep(int $cep)
+    {
+        $sql = "SELECT * FROM logradouro WHERE cep = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $cep);
+        $stmt->execute();
+
+        $endereco_obj = $stmt->fetchObject("ApiCep\Model\EnderecoModel");
+
+        //$endereco_obj->arr_cidades = $this->selectCidadesByUf($endereco_obj->UF);
+
+        return $endereco_obj;
+    }
 }
