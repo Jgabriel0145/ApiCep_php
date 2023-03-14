589e0a4b71579d6a34a02df3c3136beeb34f424f
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-03-14 15:49:54 -0300

    Atualização EnderecoModel

--- Model/EnderecoModel.php
+++ Model/EnderecoModel.php
@@ -24,5 +24,45 @@ class EnderecoModel extends Model
         }
     }
 
+    public function getLogradouroByCep(int $cep)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
 
+            return $dao->selectByCep($cep);
+        }
+        catch (Exception $e)
+        {
+            throw $e;
+        }
+    }
+
+    public function getCepByLogradouro($logradouro)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
+
+            $this->rows = $dao->selectCepByLogradouro($logradouro);
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+
+    public function getBairrosByIdCidade($id_cidade)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
+
+            $this->rows = $dao->selectBairroByIdCidade($id_cidade);
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }
 }
