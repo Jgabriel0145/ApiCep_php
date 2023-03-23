5fb1b4b136c969c392b7a1070b930b56764ee213
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-03-23 15:25:01 -0300

    CidadeModel

--- Model/CidadeModel.php
+++ Model/CidadeModel.php
@@ -2,10 +2,16 @@
 
 namespace ApiCep\Model;
 
+use ApiCep\DAO\EnderecoDAO;
+
 class CidadeModel extends Model
 {
+    public $id_cidade, $descricao, $uf, $codigo_ibge, $ddd;
+
     public function getCidadesByUF($uf)
     {
-        //...
+        $dao = new EnderecoDAO();
+
+        $this->rows = $dao->selectCidadesByUf($uf);
     }
 }
